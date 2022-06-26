2531a3575b5c9d17a38663ff1be22e39dd6aa336
ubaifadhli <ubaifadhli@it.student.pens.ac.id>
2022-06-27 00:41:16 +0700

    [FPJ-25] [FPJ-17] Element handling

--- src/main/java/com/github/ubaifadhli/util/Element.java
+++ src/main/java/com/github/ubaifadhli/util/Element.java
@@ -1,24 +1,26 @@
 package com.github.ubaifadhli.util;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import lombok.Builder;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 @Builder
 public class Element {
     private final long DEFAULT_WAIT_DURATION = 30;
-
     private By webLocator;
     private By mobileLocator;
     private int[] mobileCoordinate;
-
-    private WebElement webElement;
-    private WebElement mobileElement;
 
     private WebDriver getWebDriver() {
         return CurrentThreadDriver.getWebDriver();
@@ -28,151 +30,105 @@ public class Element {
         return CurrentThreadDriver.getMobileDriver();
     }
 
+    private WebElement getWebElement() {
+        return getWebDriver().findElement(webLocator);
+    }
+
+    private MobileElement getMobileElement() {
+        return (MobileElement) getMobileDriver().findElement(mobileLocator);
+    }
+
+    private List<WebElement> asElements() {
+        if (validToRunOnWeb())
+            return getWebDriver().findElements(webLocator);
+
+        else
+            return getMobileDriver().findElements(mobileLocator);
+    }
+
     public void click() {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
+        if (validToRunOnWeb())
+            getWebElement().click();
 
-            webElement.click();
-            webElement = null;
-
-        } else if (validToRunOnMobile()) {
-            if (mobileUseCoordinate()) {
+        else if (validToRunOnMobile())
+            if (mobileUseCoordinate())
                 forMobile().tap(mobileCoordinate[0], mobileCoordinate[1]);
-
-            } else {
-                if (mobileElement == null)
-                    mobileElement = getMobileDriver().findElement(mobileLocator);
-
-                mobileElement.click();
-                mobileElement = null;
-            }
-
-        }
+            else
+                getMobileElement().click();
     }
 
     private boolean mobileUseCoordinate() {
-        return mobileCoordinate != null;
+        return mobileCoordinate != null && mobileCoordinate.length > 1;
     }
 
     public void pressEnter() {
         if (validToRunOnWeb())
-            webElement.sendKeys(Keys.ENTER);
-        else
+            getWebElement().sendKeys(Keys.ENTER);
+
+        else if (validToRunOnMobile())
             new MobileElementFunction(getMobileDriver()).pressEnter();
     }
 
     public void clear() {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
+        if (validToRunOnWeb())
+            getWebElement().clear();
 
-            webElement.clear();
-
-        } else if (validToRunOnMobile()) {
-            if (mobileElement == null)
-                mobileElement = getMobileDriver().findElement(mobileLocator);
-
-            mobileElement.clear();
-
-        }
+        else if (validToRunOnMobile())
+            getMobileElement().clear();
     }
 
     public void clearAndType(String text) {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
-
-            webElement.clear();
-            webElement.sendKeys(text);
-
-        } else if (validToRunOnMobile()) {
-            if (mobileElement == null)
-                mobileElement = getMobileDriver().findElement(mobileLocator);
-
-            mobileElement.clear();
-            mobileElement.sendKeys(text);
-
-        }
+        clear();
+        typeIntoField(text);
     }
 
     public void typeIntoField(String text) {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
+        if (validToRunOnWeb())
+            getWebElement().sendKeys(text);
 
-            webElement.sendKeys(text);
-
-        } else if (validToRunOnMobile()) {
-            if (mobileElement == null)
-                mobileElement = getMobileDriver().findElement(mobileLocator);
-
-            mobileElement.sendKeys(text);
-
-        }
+        else if (validToRunOnMobile())
+            getMobileElement().sendKeys(text);
     }
 
     public Element waitUntilClickable() {
-        if (validToRunOnWeb()) {
-            WebDriverWait webDriverWait = new WebDriverWait(getWebDriver(), DEFAULT_WAIT_DURATION);
-            webElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(webLocator));
+        By currentLocator = validToRunOnWeb() ? webLocator : mobileLocator;
+        WebDriver currentDriver = validToRunOnWeb() ? getWebDriver() : getMobileDriver();
 
-        } else if (validToRunOnMobile()) {
-            WebDriverWait webDriverWait = new WebDriverWait(getMobileDriver(), DEFAULT_WAIT_DURATION);
-            mobileElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(mobileLocator));
-
-        }
+        WebDriverWait webDriverWait = new WebDriverWait(currentDriver, DEFAULT_WAIT_DURATION);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(currentLocator));
 
         return this;
     }
 
     public Element waitUntilVisible() {
-        if (validToRunOnWeb()) {
-            WebDriverWait webDriverWait = new WebDriverWait(getWebDriver(), DEFAULT_WAIT_DURATION);
-            webElement = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(webLocator));
+        By currentLocator = validToRunOnWeb() ? webLocator : mobileLocator;
+        WebDriver currentDriver = validToRunOnWeb() ? getWebDriver() : getMobileDriver();
 
-        } else if (validToRunOnMobile()) {
-            WebDriverWait webDriverWait = new WebDriverWait(getMobileDriver(), DEFAULT_WAIT_DURATION);
-            mobileElement = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(mobileLocator));
-
-        }
+        WebDriverWait webDriverWait = new WebDriverWait(currentDriver, DEFAULT_WAIT_DURATION);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(currentLocator));
 
         return this;
     }
 
     public void typeAndEnter(String text) {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
+        typeIntoField(text);
 
-            webElement.sendKeys(text);
-            webElement.sendKeys(Keys.ENTER);
+        if (validToRunOnWeb())
+            getWebElement().sendKeys(Keys.ENTER);
 
-        } else if (validToRunOnMobile()) {
-            if (mobileElement == null)
-                mobileElement = getMobileDriver().findElement(mobileLocator);
-
-            mobileElement.sendKeys(text);
+        else if (validToRunOnMobile())
             ((AndroidDriver) getMobileDriver()).pressKey(new KeyEvent(AndroidKey.ENTER));
-        }
     }
 
     public String getText() {
-        if (validToRunOnWeb()) {
-            if (webElement == null)
-                webElement = getWebDriver().findElement(webLocator);
+        if (validToRunOnWeb())
+            return getWebElement().getText();
 
-            return webElement.getText();
+        else if (validToRunOnMobile())
+            return getMobileElement().getText();
 
-        } else if (validToRunOnMobile()) {
-            if (mobileElement == null)
-                mobileElement = getMobileDriver().findElement(mobileLocator);
-
-            return mobileElement.getText();
-
-        } else {
+        else
             throw new RuntimeException("Element did not run on any platform because locator has not been set up.");
-        }
     }
 
     public WebElementFunction forWeb() {
@@ -195,5 +151,3 @@ public class Element {
         return CurrentThreadDriver.isCurrentPlatformMobile() && mobileLocator != null;
     }
 }
-
-
